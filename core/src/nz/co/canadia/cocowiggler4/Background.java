bbd242958b4b916fdc453ae38e347ab1a1c61110
Ashley Noel Hinton <ashley@canadia.co.nz>
2017-04-30 20:33:10 +1200

    refactor background code to a class

---
+++ core/src/nz/co/canadia/cocowiggler4/Background.java
@@ -0,0 +1,34 @@
+package nz.co.canadia.cocowiggler4;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * This is just the background image. Doesn't do much, but it knows it loves you.
+ */
+
+class Background {
+    private Texture bitmap;
+    private Sprite sprite;
+
+    Background() {
+        bitmap = new Texture(Gdx.files.internal("grass.png"));
+        bitmap.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        TextureRegion grassRegion = new TextureRegion(bitmap, 0, 0, bitmap.getWidth(),
+                bitmap.getHeight());
+        sprite = new Sprite(grassRegion);
+        sprite.setOrigin(0, 0);
+        sprite.setPosition(0, 0);
+    }
+
+    void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    void dispose() {
+        bitmap.dispose();
+    }
+}
--- core/src/nz/co/canadia/cocowiggler4/CocoWiggler.java
+++ core/src/nz/co/canadia/cocowiggler4/CocoWiggler.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -15,15 +12,13 @@ import nz.co.canadia.cocowiggler4.util.Constants;
 
 public class CocoWiggler extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture grassImage;
     private OrthographicCamera camera;
     private Viewport viewport;
     private Coco coco;
-    private Sprite grass;
+    private Background background;
 
     @Override
     public void create() {
-
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Constants.APP_WIDTH, Constants.APP_HEIGHT);
         viewport = new FitViewport(Constants.APP_WIDTH, Constants.APP_HEIGHT, camera);
@@ -32,46 +27,44 @@ public class CocoWiggler extends ApplicationAdapter {
 
         coco = new Coco();
 
-        grassImage = new Texture(Gdx.files.internal("grass.png"));
-        grassImage.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-        TextureRegion grassRegion = new TextureRegion(grassImage, 0, 0, grassImage.getWidth(),
-                grassImage.getHeight());
-        grass = new Sprite(grassRegion);
-        grass.setOrigin(0, 0);
-        grass.setPosition(0, 0);
+        background = new Background();
     }
 
     @Override
     public void render() {
+        // clear screen
         Gdx.gl.glClearColor(0.246f, 0.574f, 0.102f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // update camera
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
+        // update Coco
         coco.update(camera);
 
+        // start sprite batch
         batch.begin();
 
         // draw grass
-        grass.draw(batch);
+        background.draw(batch);
 
         // draw coco
         coco.draw(batch);
 
+        // end sprite batch
         batch.end();
     }
 
     @Override
     public void dispose() {
+        background.dispose();
         batch.dispose();
         coco.dispose();
-        grassImage.dispose();
     }
 
     @Override
     public void resize(int width, int height) {
-
         viewport.update(width, height);
     }
 }
