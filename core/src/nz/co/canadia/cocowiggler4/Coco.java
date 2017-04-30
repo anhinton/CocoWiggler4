798b601ab57fcd667bfc56869479606eef1f0a13
Ashley Noel Hinton <ashley@canadia.co.nz>
2017-04-30 20:19:10 +1200

    make Coco a class

---
+++ core/src/nz/co/canadia/cocowiggler4/Coco.java
@@ -0,0 +1,177 @@
+package nz.co.canadia.cocowiggler4;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+
+import nz.co.canadia.cocowiggler4.util.Constants;
+
+/**
+ * Coco is the star of the game, the whole reason we show up.
+ */
+
+class Coco {
+    private Texture bitmap;
+    private Sprite sprite;
+    private float rot;
+    private boolean pressingLeft;
+    private boolean pressingRight;
+    private boolean pressingUp;
+    private boolean pressingDown;
+    private boolean moving;
+    private boolean facingRight;
+    private Vector3 target;
+
+    Coco() {
+        // initialize variables
+        pressingLeft = false;
+        pressingRight = false;
+        pressingUp = false;
+        pressingDown = false;
+
+        facingRight = false;
+
+        moving = false;
+        target = new Vector3();
+
+        bitmap = new Texture(Gdx.files.internal("coco.png"));
+        bitmap.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        TextureRegion region = new TextureRegion(bitmap, 0, 0, bitmap.getWidth(),
+                bitmap.getHeight());
+        sprite = new Sprite(region);
+        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+        sprite.setPosition(Constants.APP_WIDTH / 2 - bitmap.getWidth() / 2,
+                Constants.APP_HEIGHT / 2 - bitmap.getHeight() / 2);
+    }
+
+    void update(Camera camera) {
+        // Movement controls
+
+        // Keyboard movement
+        pressingLeft = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+        pressingRight = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+        pressingUp = Gdx.input.isKeyPressed(Input.Keys.UP);
+        pressingDown = Gdx.input.isKeyPressed(Input.Keys.DOWN);
+
+        if (pressingLeft) {
+            if (facingRight) {
+                facingRight = false;
+            }
+            if (pressingUp) {
+                sprite.setX(sprite.getX() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+                sprite.setY(sprite.getY() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+            } else if (pressingDown) {
+                sprite.setX(sprite.getX() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+                sprite.setY(sprite.getY() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+
+            } else {
+                sprite.setX(sprite.getX() - Constants.SPEED * Gdx.graphics.getDeltaTime());
+            }
+        }
+        if (pressingRight) {
+            if (!facingRight) {
+                facingRight = true;
+            }
+            if (pressingUp) {
+                sprite.setX(sprite.getX() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+                sprite.setY(sprite.getY() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+            } else if (pressingDown) {
+                sprite.setX(sprite.getX() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+                sprite.setY(sprite.getY() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
+
+            } else {
+                sprite.setX(sprite.getX() + Constants.SPEED * Gdx.graphics.getDeltaTime());
+            }
+        }
+        if (pressingUp & !pressingLeft & !pressingRight) {
+            sprite.setY(sprite.getY() + Constants.SPEED * Gdx.graphics.getDeltaTime());
+        }
+        if (pressingDown & !pressingLeft & !pressingRight) {
+            sprite.setY(sprite.getY() - Constants.SPEED * Gdx.graphics.getDeltaTime());
+        }
+
+        // Mouse/touch-screen movement
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+            moving = true;
+            camera.unproject(target.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+            // stop target from clipping Coco outside
+            if (target.x < sprite.getWidth() / 2) target.x = sprite.getWidth() / 2;
+            if (target.x > Constants.APP_WIDTH - sprite.getWidth() / 2)
+                target.x = Constants.APP_WIDTH - sprite.getWidth() / 2;
+            if (target.y < sprite.getHeight() / 2) target.y = sprite.getHeight() / 2;
+            if (target.y > Constants.APP_HEIGHT - sprite.getHeight() / 2)
+                target.y = Constants.APP_HEIGHT - sprite.getHeight() / 2;
+
+
+        }
+        if (moving) {
+            if (Math.abs(sprite.getX() - target.x + sprite.getWidth() / 2) < 5
+                    & Math.abs(sprite.getY() - target.y + sprite.getHeight() / 2) < 5) {
+                moving = false;
+            } else {
+                move();
+            }
+
+        }
+
+        // don't let Coco escape
+        if (sprite.getX() < 0) sprite.setX(0);
+        if (sprite.getX() > Constants.APP_WIDTH - sprite.getWidth())
+            sprite.setX(Constants.APP_WIDTH - sprite.getWidth());
+        if (sprite.getY() < 0) sprite.setY(0);
+        if (sprite.getY() > Constants.APP_HEIGHT - sprite.getHeight())
+            sprite.setY(Constants.APP_HEIGHT - sprite.getHeight());
+
+        // set flip
+        sprite.setFlip(facingRight, false);
+    }
+
+    void draw(SpriteBatch batch) {
+        rotate();
+        sprite.draw(batch);
+    }
+
+    void dispose() {
+        bitmap.dispose();
+    }
+
+    private void move() {
+        // calculate distance along adjacent and opposite sides
+        float pathX = target.x - sprite.getWidth() / 2 - sprite.getX();
+        float pathY = target.y - sprite.getHeight() / 2 - sprite.getY();
+
+        // flip Coco on direction change
+        if (pathX < 0 && facingRight) {
+            facingRight = false;
+        }
+        if (pathX > 0 && !facingRight) {
+            facingRight = true;
+        }
+
+        // calculate distance along hypotenuse
+        float distance = (float) Math.sqrt(pathX * pathX + pathY * pathY);
+        // change is ratio between side an hypotenuse
+        float changeX = pathX / distance;
+        float changeY = pathY / distance;
+
+        // move Coco
+        sprite.setX(sprite.getX() + changeX * Constants.SPEED * Gdx.graphics.getDeltaTime());
+        sprite.setY(sprite.getY() + changeY * Constants.SPEED * Gdx.graphics.getDeltaTime());
+    }
+
+    private void rotate() {
+        final float degreesPerSecond = 10.0f;
+        rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 360;
+        final float shakeAmplitudeInDegrees = 5.0f;
+        float shake = MathUtils.sin(rot) * shakeAmplitudeInDegrees;
+        sprite.setRotation(shake);
+    }
+
+}
--- core/src/nz/co/canadia/cocowiggler4/CocoWiggler.java
+++ core/src/nz/co/canadia/cocowiggler4/CocoWiggler.java
@@ -2,15 +2,12 @@ package nz.co.canadia.cocowiggler4;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -18,36 +15,14 @@ import nz.co.canadia.cocowiggler4.util.Constants;
 
 public class CocoWiggler extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture cocoImage;
     private Texture grassImage;
     private OrthographicCamera camera;
     private Viewport viewport;
-    private Sprite coco;
+    private Coco coco;
     private Sprite grass;
-    private float rot;
-    private boolean pressingLeft;
-    private boolean pressingRight;
-    private boolean pressingUp;
-    private boolean pressingDown;
-    private boolean seekingTarget;
-    private boolean facingRight;
-    private Vector3 targetXYZ;
-    private float pathX;
-    private float pathY;
-    private float distance;
-    private float changeX;
-    private float changeY;
 
     @Override
     public void create() {
-        pressingLeft = false;
-        pressingRight = false;
-        pressingUp = false;
-        pressingDown = false;
-        facingRight = false;
-
-        seekingTarget = false;
-        targetXYZ = new Vector3();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Constants.APP_WIDTH, Constants.APP_HEIGHT);
@@ -55,14 +30,7 @@ public class CocoWiggler extends ApplicationAdapter {
 
         batch = new SpriteBatch();
 
-        cocoImage = new Texture(Gdx.files.internal("coco.png"));
-        cocoImage.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-        TextureRegion cocoRegion = new TextureRegion(cocoImage, 0, 0, cocoImage.getWidth(),
-                cocoImage.getHeight());
-        coco = new Sprite(cocoRegion);
-        coco.setOrigin(coco.getWidth() / 2, coco.getHeight() / 2);
-        coco.setPosition(Constants.APP_WIDTH / 2 - cocoImage.getWidth() / 2,
-                Constants.APP_HEIGHT / 2 - cocoImage.getHeight() / 2);
+        coco = new Coco();
 
         grassImage = new Texture(Gdx.files.internal("grass.png"));
         grassImage.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
@@ -81,19 +49,7 @@ public class CocoWiggler extends ApplicationAdapter {
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
-        // don't let Coco go off-screen
-        if (coco.getX() < 0) {
-            coco.setX(0);
-        }
-        if (coco.getX() + coco.getWidth() > Constants.APP_WIDTH) {
-            coco.setX(Constants.APP_WIDTH - coco.getWidth());
-        }
-        if (coco.getY() < 0) {
-            coco.setY(0);
-        }
-        if (coco.getY() + coco.getHeight() > Constants.APP_HEIGHT) {
-            coco.setY(Constants.APP_HEIGHT - coco.getHeight());
-        }
+        coco.update(camera);
 
         batch.begin();
 
@@ -101,112 +57,15 @@ public class CocoWiggler extends ApplicationAdapter {
         grass.draw(batch);
 
         // draw coco
-        final float degreesPerSecond = 10.0f;
-        rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 360;
-        final float shakeAmplitudeInDegrees = 5.0f;
-        float shake = MathUtils.sin(rot) * shakeAmplitudeInDegrees;
-        coco.setRotation(shake);
         coco.draw(batch);
 
         batch.end();
-
-        // Movement controls
-
-        // Keyboard movement
-        pressingLeft = Gdx.input.isKeyPressed(Input.Keys.LEFT);
-        pressingRight = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
-        pressingUp = Gdx.input.isKeyPressed(Input.Keys.UP);
-        pressingDown = Gdx.input.isKeyPressed(Input.Keys.DOWN);
-
-        if (pressingLeft) {
-            if (facingRight) {
-                facingRight = false;
-            }
-            if (pressingUp) {
-                coco.setX(coco.getX() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-                coco.setY(coco.getY() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-            } else if (pressingDown) {
-                coco.setX(coco.getX() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-                coco.setY(coco.getY() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-
-            } else {
-                coco.setX(coco.getX() - Constants.SPEED * Gdx.graphics.getDeltaTime());
-            }
-        }
-        if (pressingRight) {
-            if (!facingRight) {
-                facingRight = true;
-            }
-            if (pressingUp) {
-                coco.setX(coco.getX() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-                coco.setY(coco.getY() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-            } else if (pressingDown) {
-                coco.setX(coco.getX() + Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-                coco.setY(coco.getY() - Constants.ANGLE_SPEED * Gdx.graphics.getDeltaTime());
-
-            } else {
-                coco.setX(coco.getX() + Constants.SPEED * Gdx.graphics.getDeltaTime());
-            }
-        }
-        if (pressingUp & !pressingLeft & !pressingRight) {
-            coco.setY(coco.getY() + Constants.SPEED * Gdx.graphics.getDeltaTime());
-        }
-        if (pressingDown & !pressingLeft & !pressingRight) {
-            coco.setY(coco.getY() - Constants.SPEED * Gdx.graphics.getDeltaTime());
-        }
-
-        // Mouse/touch-screen movement
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            seekingTarget = true;
-            camera.unproject(targetXYZ.set(Gdx.input.getX(), Gdx.input.getY(), 0));
-            if (targetXYZ.x < coco.getWidth() / 2) {
-                targetXYZ.x = coco.getWidth() / 2;
-            }
-            if (targetXYZ.x > Constants.APP_WIDTH - coco.getWidth() / 2) {
-                targetXYZ.x = Constants.APP_WIDTH - coco.getWidth() / 2;
-            }
-            if (targetXYZ.y < coco.getHeight() / 2) {
-                targetXYZ.y = coco.getHeight() / 2;
-            }
-            if (targetXYZ.y > Constants.APP_HEIGHT - coco.getHeight() / 2) {
-                targetXYZ.y = Constants.APP_HEIGHT - coco.getHeight() / 2;
-            }
-
-            pathX = targetXYZ.x - coco.getWidth() / 2 - coco.getX();
-            pathY = targetXYZ.y - coco.getHeight() / 2 - coco.getY();
-
-            // flip Coco on direction change
-            if (pathX < 0 && facingRight) {
-                facingRight = false;
-            }
-            if (pathX > 0 && !facingRight) {
-                facingRight = true;
-            }
-
-            distance = (float) Math.sqrt(pathX * pathX + pathY * pathY);
-            changeX = pathX / distance;
-            changeY = pathY / distance;
-        }
-        if (seekingTarget) {
-            if (Math.abs(coco.getX() - targetXYZ.x + coco.getWidth() / 2) < 5
-                    & Math.abs(coco.getY() - targetXYZ.y + coco.getHeight() / 2) < 5) {
-                seekingTarget = false;
-            } else {
-
-                coco.setX(coco.getX() + changeX * Constants.SPEED * Gdx.graphics.getDeltaTime());
-                coco.setY(coco.getY() + changeY * Constants.SPEED * Gdx.graphics.getDeltaTime());
-            }
-
-        }
-
-        // set flip
-        coco.setFlip(facingRight, false);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        cocoImage.dispose();
+        coco.dispose();
         grassImage.dispose();
     }
 
