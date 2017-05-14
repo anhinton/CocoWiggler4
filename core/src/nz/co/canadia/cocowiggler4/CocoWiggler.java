ad2cabfe1df767cb36d1e0d61b8638a1723b512c
Ashley Noel Hinton <ashley@canadia.co.nz>
2017-05-14 21:03:36 +1200

    add backing music

--- core/src/nz/co/canadia/cocowiggler4/CocoWiggler.java
+++ core/src/nz/co/canadia/cocowiggler4/CocoWiggler.java
@@ -1,15 +1,26 @@
 package nz.co.canadia.cocowiggler4;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class CocoWiggler extends Game {
 
     SpriteBatch batch;
+    private Music music;
 
     public void create() {
         batch = new SpriteBatch();
+
+        // play music
+        music = Gdx.audio.newMusic(Gdx.files.internal("music/soundtrack.ogg"));
+        music.setVolume(0.5f);
+        music.setLooping(true);
+        music.play();
+
+        // show the intro screen
         this.setScreen(new SplashScreen(this));
     }
 
@@ -18,6 +29,7 @@ public class CocoWiggler extends Game {
     }
 
     public void dispose() {
+        music.dispose();
         batch.dispose();
     }
 
