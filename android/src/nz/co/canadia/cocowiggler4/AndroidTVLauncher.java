84d9334a1fd87d3077575d85e015e861c977a715
Ashley Noel Hinton <ashley@canadia.co.nz>
2018-03-04 12:13:46 +1300

    this game now works on Android TV because I did these changes

---
+++ android/src/nz/co/canadia/cocowiggler4/AndroidTVLauncher.java
@@ -0,0 +1,9 @@
+package nz.co.canadia.cocowiggler4;
+
+/**
+ * This class simply inherits everything from AndroidLauncher but with a different name
+ * because I'm not really sure what I'm doing.
+ */
+
+public class AndroidTVLauncher extends AndroidLauncher {
+}
